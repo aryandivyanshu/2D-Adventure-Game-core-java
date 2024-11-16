ce574d1d4915d6c1974cb4774a75de4954b111e9
Aryan Divyanshu <52078579+aryandivyanshu@users.noreply.github.com>
2024-11-16 21:29:43 +0530

    Add files via upload

---
+++ KeyHandler.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener { //KeyListener is the interface to receive keyboard input or keystrokes.
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}

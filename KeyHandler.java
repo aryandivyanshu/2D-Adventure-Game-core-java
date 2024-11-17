eba2bf1d07f2202650d5e076ed8de909196cad83
Aryan Divyanshu <52078579+aryandivyanshu@users.noreply.github.com>
2024-11-17 09:11:30 +0530

    Update KeyHandler.java

--- KeyHandler.java
+++ KeyHandler.java
@@ -4,6 +4,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener { //KeyListener is the interface to receive keyboard input or keystrokes.
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -12,10 +15,42 @@ public class KeyHandler implements KeyListener { //KeyListener is the interface 
     @Override
     public void keyPressed(KeyEvent e) {
 
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_S){
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_A){
+            rightPressed = true;
+        }
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_S){
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            rightPressed = false;
+        }
+
+
 
     }
 }
